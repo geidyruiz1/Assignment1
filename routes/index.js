e623c262d1b69f564e741612b35297ebbd2c7d29
Diego Estrada Beltran <86diegoestrada@gmail.com>
2020-11-13 00:33:23 +0000

    edit GUI and messages from index.js to hbs files, edit of css also

--- routes/index.js
+++ routes/index.js
@@ -8,23 +8,31 @@ router.get('/', (req, res, next) => {
     res.render('index', { title: 'Home' });
 });
 
-/* GET Gelvis page. */
+/* GET about page. */
 router.get('/about', (req, res, next) => {
-    res.render('about', { message1: 'This website allows users to add products and specify the kind of cathegory of the product' });
+    res.render('about', {
+        message1: 'This website allows users to add products and specify the kind of cathegory of the product',
+    message2:'We are a company that offer logistic solutions to manage all the products that a company sells in one platforms'});
 });
 
-/* GET Gloria page. */
+/* GET projects page. */
 router.get('/projects', (req, res, next) => {
-    res.render('projects', { message2: 'Projects developed' });
+    res.render('projects', { message2: 'Our Staff develop all the necesarry projects to help your company to work better' });
 });
 
-/* GET Geidy page. */
+/* GET services page. */
 router.get('/services', (req, res, next) => {
     res.render('services', { message3: 'Offered' });
 });
-/* GET Ximena page. */
+/* GET contact page. */
 router.get('/contact', (req, res, next) => {
-    res.render('contact', { message4: 'Contact us' });
+    res.render('contact', { message4: 'The easy way to Contact our company is using this bike to drive to our location or with just a mail' });
 });
+
+/* GET contact page. */
+router.get('/produts', (req, res, next) => {
+    res.render('produts', { message1: 'This Website Allow users to Modify Products' });
+});
+
 //express this file as public
 module.exports = router;
